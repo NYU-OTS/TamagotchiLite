a6954db5fe8e53b47bb04f109399d01b5f707d91
Rebecca Hom <rebecca.hom@nyu.edu>
2018-06-12 13:45:45 -0400

    Made new constants

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -15,3 +15,12 @@ export type HUNGER_DEPLETE = typeof HUNGER_DEPLETE;
 
 export const HAPPY_DEPLETE = 'HAPPY_DEPLETE';
 export type HAPPY_DEPLETE = typeof HAPPY_DEPLETE;
+
+export const SET_HUNGER = 'SET_HUNGER';
+export type SET_HUNGER = typeof SET_HUNGER;
+
+export const SET_HAPPINESS = 'SET_HAPPINESS';
+export type SET_HAPPINESS = typeof SET_HAPPINESS;
+
+export const SET_MONEY = 'SET_MONEY';
+export type SET_MONEY = typeof SET_MONEY;
